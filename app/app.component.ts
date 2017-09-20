a2e1e8a4994024da20d80574a0ecc8b6f34e517a
Aman Shukla <amanshukla18.as@gmail.com>
2017-09-20 13:24:52 +0530

    Angular  2  skeleton
    Angular 2 skeleton with latest dependencies

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+// This import statement must be in all component.ts files
+import { Component } from  '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>Skeleton Project</h1>'
+})
+
+export class AppComponent { }
